a4ef1259dd8cb5632e99515e01dd7a9ee2dda931
anna-kushnir <anna.kushnir.1008@gmail.com>
2022-02-08 00:08:09 +0200

    Add project files.

---
+++ OP_Lab1_Cpp/Lab1_functions.cpp
@@ -0,0 +1,2 @@
+#include "header.h"
+using namespace std;
---
+++ OP_Lab1_Cpp/Lab1_main.cpp
@@ -0,0 +1,12 @@
+#include "header.h"
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+
+
+	system("pause");
+	return 0;
+}
