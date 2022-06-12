b0d7d81f2dff86d3d1e5f54df28cc74fdadff37c
anna-kushnir <anna.kushnir.1008@gmail.com>
2022-06-13 00:31:03 +0300

    Лаб. р. 5 (дерева)

--- OP_Lab6_Cpp/Lab6_main.cpp
+++ OP_Lab6_Cpp/Lab6_main.cpp
@@ -10,14 +10,14 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	ifstream file("text.txt");					// Вхідний файл з виразом.
+	ifstream file("text.txt");                  // Вхідний файл з виразом.
 	if (!file.is_open()) {
 		cout << "Не вдалося відкрити файл для зчитування виразу!\n\n";
 		return -1;
 	}
-	vector<string> mass = readFile(file);		// Масив зчитаних з виразу символів.
+	vector<string> mass = readFile(file);       // Масив зчитаних з виразу символів.
 	file.close();
-	ofstream outFile("result.txt");				// Результуючий файл.
+	ofstream outFile("result.txt");             // Результуючий файл.
 
 	if (mass[0] == "") {
 		cout << "Неправильне введення формули!\n\n";
@@ -29,13 +29,13 @@ int main()
 	outFile << "Введений вираз:\n";
 	outputVector(mass, outFile);
 
-	int i = 0;		// Змінна для визначення індексу поточного елементу вектора mass.
+	int i = 0;        // Змінна для визначення індексу поточного елементу вектора mass.
 	Node* Tree = makeTree(mass, i);
 
 	outFile << "\n\nПобудоване дерево-формула:\n\n";
 	outputTree(Tree, 0, outFile);
 
-	double result = count(Tree);				// Результат обчислення виразу.
+	double result = count(Tree);                // Результат обчислення виразу.
 	outFile << "\n\nРезультат обчислення формули: " << result;
 	cout << "Побудоване дерево та отриманий результат були успішно виведені у файл.\n\n";
 
